cd1e616c11956dc74fb9350137528dd722e2ae84
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-03-11 10:56:41 +0530

    ADDED: Code and test to find top balling average

--- src/main/java/IPLAdapter.java
+++ src/main/java/IPLAdapter.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 import com.jarpackage.CSVBuilderFactory;
 import com.jarpackage.ICSVBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -9,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
 
 
 public class IPLAdapter {
@@ -25,41 +25,39 @@ public class IPLAdapter {
         this.sortMap.put(SortField.FOURS_AND_SIX, Comparator.comparing(census -> census.fours + census.six));
         this.sortMap.put(SortField.STRIKING_RATES_WITH_FOURS_AND_SIX, Comparator.comparing(census -> census.fours + census.six));
         this.sortMap.put(SortField.STRIKING_RATES_WITH_AVERAGES, Comparator.comparing(census -> census.strikeRates + census.average));
-        this.sortMap.put(SortField.OVERS, Comparator.comparing(census -> census.overs));
-        this.sortMap.put(SortField.AVG, Comparator.comparing(census -> census.average));
-
+        this.sortMap.put(SortField.BALLING_AVERAGE, Comparator.comparing(census -> census.average));
     }
 
-    //  public <E> Map<String, CensusDTO> loadCensusData(Class<E> CensusCSVClass, String... csvFilePath)
-    public <E> String loadCensusData(SortField sortField, String csvFilePath, Class<E> CensusCSVClass) throws IOException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+  //  public <E> Map<String, CensusDTO> loadCensusData(Class<E> CensusCSVClass, String... csvFilePath)
+  public <E> String loadCensusData(SortField sortField, String csvFilePath, Class<E> CensusCSVClass ) throws IOException {
+      try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
 
-            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<E> CSVStateIterator = csvBuilder.getCSVIterator(reader, CensusCSVClass);
-            Iterable<E> csvIterable = () -> CSVStateIterator;
+          ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+          Iterator<E> CSVStateIterator = csvBuilder.getCSVIterator(reader, CensusCSVClass);
+          Iterable<E> csvIterable = () -> CSVStateIterator;
+          System.out.println("FILED IS" +sortField);
+          if (CensusCSVClass.getName().equals("IPLRunsPOJO")) {
+              StreamSupport.stream(csvIterable.spliterator(), false)
+                      .map(IPLRunsPOJO.class::cast)
+                      .forEach(censusCSV -> iplMap.put(censusCSV.player, new IPLDTO(censusCSV)));
+          }
 
-            if (CensusCSVClass.getName().equals("IPLRunsPOJO")) {
-                StreamSupport.stream(csvIterable.spliterator(), false)
-                        .map(IPLRunsPOJO.class::cast)
-                        .forEach(censusCSV -> iplMap.put(censusCSV.player, new IPLDTO(censusCSV)));
-            }
+          if (CensusCSVClass.getName().equals("IPLWicketsPOJO")) {
+              StreamSupport.stream(csvIterable.spliterator(), false)
+                      .map(IPLWicketsPOJO.class::cast)
+                      .forEach(censusCSV -> iplMap.put(censusCSV.player, new IPLDTO(censusCSV)));
+          }
 
-            if (CensusCSVClass.getName().equals("IPLWicketsPOJO")) {
-                StreamSupport.stream(csvIterable.spliterator(), false)
-                        .map(IPLWicketsPOJO.class::cast)
-                        .forEach(censusCSV -> iplMap.put(censusCSV.player, new IPLDTO(censusCSV)));
-            }
+          if (iplMap == null || iplMap.size() == 0) {
+              throw new IPLException("no census data", IPLException.ExceptionType.NO_CENSUS_DATA);
+          }
 
-            if (iplMap == null || iplMap.size() == 0) {
-                throw new IPLException("no census data", IPLException.ExceptionType.NO_CENSUS_DATA);
-            }
-
-            IPLDTOList = iplMap.values().stream().collect(Collectors.toList());
-            this.sort(this.sortMap.get(sortField).reversed());
-            String sortedData = new Gson().toJson(this.IPLDTOList);
-            return sortedData;
-        }
-    }
+          IPLDTOList = iplMap.values().stream().collect(Collectors.toList());
+          this.sort(this.sortMap.get(sortField).reversed());
+          String sortedData = new Gson().toJson(this.IPLDTOList);
+          return sortedData;
+      }
+  }
 
     private void sort(Comparator<IPLDTO> reversed) {
         for (int i = 0; i < IPLDTOList.size() - 1; i++) {
